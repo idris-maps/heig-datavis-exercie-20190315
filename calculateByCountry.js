2e356e481ec9fd8bcd299e22b0fbeb545126fa9d
Pauline Baeni <pauline.baeni@heig-vd.ch>
2019-03-27 19:34:05 +0100

    Corrected errors

--- calculateByCountry.js
+++ calculateByCountry.js
@@ -5,32 +5,48 @@ const fs = require('fs');
 
 const file = require('./asylum.json');
 
-const list = file.map(d => ({country : d.country, affected:d.affected}));
 
-const countryList= list;
-const tab = [];
-
-countryList.forEach(e => {
-    tab.push(e.country);
-})
-
-let uniq = [...new Set(tab)];
-
-const tab2 = [];
-uniq.forEach(e => {
-    tab2.push({country: e, sum: 0})
-})
-
-let l = list.length;
-
-for (let i = 0; i < l; i++ ){
-    tab2.forEach(e => {
-        if (e.country == list[i].country) {
-            e.sum = e.sum + list[i].affected;
-        }
-    })
+const saveJSON = data => {
+  // data est la liste d'objets créés avec "formatData"
+  fs.writeFile(
+    'asylumByCountry.json', // le nom du fichier
+    JSON.stringify(data, null, 2), // les données transformées en chaîne de charactères
+    'utf-8', // l'encodage du fichier
+    err => err ? console.log(err) : console.log('Saved file')
+    // cette fonction est appelée quand le fichier a été sauvé
+    // ou si une erreur est survenue
+    // elle prends un argument "err" qui est l'erreur s'il y en a une
+    // si "err" existe, nous écrivons l'erreur dans la console
+    // sinon nous disons que le fichier a été sauvé
+  )
 }
 
-const save = data => fs.writeFile('asylumByCountry.json', JSON.stringify(data, null, 2), 'utf-8', err => err ? console.log(err): console.log('FileReady'))
+const getCountry=country=>{
+	const countries = country.map(R.prop('country'))
+	return R.uniq(countries)
+}
 
-save(tab2);
+const sumAffectedByCountry = (file, country) =>
+  file
+   .filter(d => d.country === country) // ne prendre que les éléments relatifs à ce pays
+   .map(d => d.affected) // une liste de chiffres genre [1,2,3,1]
+   .reduce((result, affected) => result + affected, 0) // faire la somme
+
+
+
+const formatData = country => {
+	const countries = getCountry(country);
+	return countries
+	.map(country=>({
+		country, 
+		affected: sumAffectedByCountry(file,country),
+	}))
+	 .sort((a, b) => b.affected - a.affected);
+}
+
+
+
+var data = (formatData(file));
+saveJSON(data);
+// var test= sumAffectedByCountry('Canada',list)
+//saveJSON(test);
--- drawGraph.js
+++ drawGraph.js
@@ -16,45 +16,46 @@ const BAR_SPACE = HEIGHT / data.length
 const BAR_HEIGHT = BAR_SPACE * 0.7
 const BAR_COLOR = 'steelblue'
 const NAME_MARGIN_LEFT = WIDTH / 50
-const NAME_COLOR = 'red'
+const NAME_COLOR = 'black'
 const SUM_MARGIN_RIGHT = WIDTH / 50
-const SUM_COLOR = 'red'
+const SUM_COLOR = 'black'
 
 const scale = d3.scaleLinear()
-  .domain([0, d3.max(data, d => d.sum)])
+  .domain([0, d3.max(data, d => d.affected)])
   .range([0, WIDTH])
 
   const drawBar = (sum, index) =>
   `<rect
     y="${index * BAR_SPACE}"
     height="${BAR_HEIGHT}"
-    width="${scale(sum)}" 
-    fill="${BAR_COLOR}"/>`
+  width="${scale(sum)}" 
+      fill="${BAR_COLOR}"/>`
 
     const drawBars = data =>
   data
     // pour chaque élément de la liste nous passons la clé "sum" et l'index à "drawBar"
-    .map((d, i) => drawBar(d.sum, i))
+    .map((d, i) => drawBar(d.affected, i))
     // "drawBar" retourne une chaîne de charactères que nous joignons avec "\n" (à la ligne)
     .join('\n')
 
-const writeArtistName = (country, index) =>
+
+const writeCountryName = (name, index) =>
   `<text
     x="${NAME_MARGIN_LEFT}"
     y="${index * BAR_SPACE + BAR_HEIGHT * 0.7}"
     fill="${NAME_COLOR}">
-    ${country.toUpperCase()}
+    ${name.toUpperCase()}
   </text>`
 
 // écrire tous les noms d'artistes
 const writeNames = data =>
   data
-    .map((d, i) => writeArtistName(d.country, i))
+    .map((d, i) => writeCountryName(d.country, i))
     .join('\n')
 
 
 
-    const writeNumberOfSongs = (sum, index) =>
+const writeNumberofDemands = (sum, index) =>
   `<text
     x="${scale(sum) - SUM_MARGIN_RIGHT}"
     y="${index * BAR_SPACE + BAR_HEIGHT * 0.7}"
@@ -63,19 +64,22 @@ const writeNames = data =>
     ${sum}
     </text>`
 
-    const writeNumbersOfSongs = data =>
+// écrire le nombre de chansons sur tous les bâtons
+const writeNumbersofDemands = data =>
   data
-    .map((d, i) => writeNumberOfSongs(d.sum, i))
+    .map((d, i) => writeNumberofDemands(d.affected, i))
     .join('\n')
-
 
 
     const svg = data => `
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${WIDTH} ${HEIGHT}">
     ${drawBars(data)}
-    ${writeNumbersOfSongs(data)}
+   ${writeNumbersofDemands(data)}
+      ${writeNames(data)}
+
+
   </svg>
 `
-
 // écrire le fichier "graph.svg"
-fs.writeFileSync('graph.svg', svg(data), 'utf-8')
+fs.writeFileSync('graphe.svg', svg(data), 'utf-8')
+
