955a7d21c5e8167c1cff37b3a4e2e2ee17226480
Adrien Ciampone <39824607+Skyggen@users.noreply.github.com>
2019-03-27 18:06:43 +0100

    Update calculateByCountry.js

--- calculateByCountry.js
+++ calculateByCountry.js
@@ -1,13 +1,37 @@
 const R = require('ramda')
 
 const writeJson = require('./writeJson')
-/*
-  writeJson prends deux arguments:
-    * le nom du fichier
-    * un objet ou une liste JSON
-  
-  ex:
-    writeJson('asylumByCountry.json', list)
-*/
 const DATA = require('./asylum.json')
-// asylum.json doit être créé avec prepareData
+const fetch = require('node-fetch');
+const d3 = require('d3');
+const fs = require('fs');
+const DATA = require('./asylum.json');
+const list = DATA.map(d => ({country : d.country, affected:d.affected}));
+const countryList= list;
+const tab = [];
+const tab2 = [];
+
+countryList.forEach(e => {
+    tab.push(e.country);
+})
+
+let uniq = [...new Set(tab)];
+
+
+uniq.forEach(e => {
+    tab2.push({country: e, sum: 0})
+})
+
+let l = list.length;
+
+for (let i = 0; i < l; i++ ){
+    tab2.forEach(e => {
+        if (e.country == list[i].country) {
+            e.sum = e.sum + list[i].affected;
+        }
+    })
+}
+
+const save = data => fs.writeFile('asylumByCountry.json', JSON.stringify(data, null, 2), 'utf-8', err => err ? console.log(err): console.log('FileReady'))
+
+save(tab2); 
