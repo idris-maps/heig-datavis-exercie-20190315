98b16db87279a0b12a323275179634d0aa906b18
Gaëtan Haefeli <gagah98@gmail.com>
2019-03-28 18:31:02 +0100

    graphique effectué

--- drawGraph.js
+++ drawGraph.js
@@ -8,10 +8,9 @@ const writeSvg = svg =>
 const DATA = require('./asylumByCountry.json');
 
 const graph = DATA => `<svg width="1920" heigth="1080" xmlns="http://www.w3.org/2000/svg">
-	${DATA.map((d,i) => `<rect x="${(i+1)*87}" y="${d.somme}"  fill="green" />
-						<text x="${(i+1)*80}" y="30" font-family="Helvetica" font-size="10">${d.p}(${d.somme})</text>`).join('\n')}
-						<text x="700" y="220" font-family="Helvetica" font-size="20">Demandes d'asiles par pays</text>
-</svg>`
+	${DATA.map((d,i) => `<rect width="30" height="${d.somme*10}" x="${(i+1)*65}" y="0" fill="green" />
+	<text x="${(i+1)*65}" y="${(d.somme*10)+15}" font-family= "monospace" font-size="10">${d.p} ${d.somme} </text>`)}
+	</svg>`
 
 
 
